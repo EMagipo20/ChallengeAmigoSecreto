f92f7dc4f5f3d5bb80e9f6815c7efa13177cb09a
EMagipo20 <U202120798@upc.edu.pe>
2025-02-27 11:04:10 -0500

    Segundo commit

--- app.js
+++ app.js
@@ -1,2 +1,25 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-//Implementacion de las funonalidades
+// Array para almacenar los nombres de los amigos
+let amigos = [];
+
+// Función para agregar amigos al array
+function agregarAmigo() {
+    // Obtener el valor del input
+    let nombreAmigo = document.getElementById('amigo').value;
+
+    // Validar que el campo no esté vacío
+    if (nombreAmigo === "") {
+        alert("Por favor, inserte un nombre.");
+        return;
+    }
+
+    // Añadir el nombre al array
+    amigos.push(nombreAmigo);
+
+    // Limpiar el campo de entrada
+    document.getElementById('amigo').value = "";
+
+    // Actualizar la lista de amigos en la interfaz
+    actualizarListaAmigos();
+}
+
